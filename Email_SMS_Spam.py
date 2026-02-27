7683e19f9b1609b6af62850369c08e4269c93079
Sankesh Lal <sankesh.lal12@gmail.com>
2026-02-27 12:15:27 +0500

    Update Email_SMS_Spam.py

--- Email_SMS_Spam.py
+++ Email_SMS_Spam.py
@@ -5,31 +5,23 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt')      
+# Download once safely
+nltk.download('punkt')
 nltk.download('stopwords')
+
 ps = PorterStemmer()
+stop_words = set(stopwords.words('english'))   # 🔥 IMPORTANT
 
 def transform_text(text):
-  text = text.lower()
-  text = nltk.word_tokenize(text)
-  y = []
-  for i in text:
-    if i.isalnum():
-      y.append(i)
+    text = text.lower()
+    text = nltk.word_tokenize(text)
 
-  text = y[:]
-  y.clear()
+    y = []
+    for i in text:
+        if i.isalnum() and i not in stop_words and i not in string.punctuation:
+            y.append(ps.stem(i))
 
-  for i in text:
-    if i not in stopwords.words('english') and i not in string.punctuation:
-      y.append(i)
-
-  text = y[:]
-  y.clear()
-  for i in text:
-    y.append(ps.stem(i))
-
-  return " ".join(y)
+    return " ".join(y)
 
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
@@ -40,17 +32,14 @@ input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
 
-    # Preprocess text
-    transformed_sms = transform_text(input_sms)
-
-    # Vectorize text
-    vector_input = tfidf.transform([transformed_sms])
-
-    # Predict
-    result = model.predict(vector_input)[0]
-
-    # Display result
-    if result == 1:
-        st.header("Spam")
+    if input_sms.strip() == "":
+        st.warning("Please enter a message")
     else:
-        st.header("Not Spam")
+        transformed_sms = transform_text(input_sms)
+        vector_input = tfidf.transform([transformed_sms])
+        result = model.predict(vector_input)[0]
+
+        if result == 1:
+            st.error("Spam")
+        else:
+            st.success("Not Spam")
